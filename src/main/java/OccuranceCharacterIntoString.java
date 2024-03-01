d50646351c0690faed4be13c83a197459c2ce966
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-03-01 11:46:47 +0530

    tendable.com

---
+++ src/main/java/OccuranceCharacterIntoString.java
@@ -0,0 +1,19 @@
+public class OccuranceCharacterIntoString {
+    public static void main(String[] args) {
+        String str="Mohammad Shahnawaz";
+
+        int len=str.length();
+        int cnt=1;
+        for(int i=0;i<len;i++){
+            for(int j=i+1;j<len;j++)
+            {
+                if(str.charAt(i)==str.charAt(j))
+                {
+                    cnt++;
+                }
+            }
+            System.out.println(str.charAt(i) + "Occured : " + cnt);
+            cnt=1;
+        }
+    }
+}
