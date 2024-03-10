20fbf386cbfdbbcd4b92e9ae508bfb82a962c658
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-03-10 22:40:42 +0530

    tendable.com

---
+++ src/main/java/org/strings/StringOperationClass.java
@@ -0,0 +1,10 @@
+package org.strings;
+
+public class StringOperationClass {
+    public static void main(String[] args) {
+
+    }
+    static void capitalSmallCharacter(String first,String second){
+
+    }
+}
