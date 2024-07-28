9afa98a4b4f2069d4757933b50c75758a95398fd
Mohammad Shahnawaz <msnazia2012@gmail.com>
2024-07-28 18:34:44 +0530

    tendable.com

---
+++ src/test/java/browsermobproxy/BrowserMobProxyExample.java
@@ -0,0 +1,11 @@
+package browsermobproxy;
+
+import org.testng.annotations.Test;
+
+public class BrowserMobProxyExample {
+
+@Test
+    public void browserMobProxyDisp(){
+    System.out.println("hi");
+}
+}
